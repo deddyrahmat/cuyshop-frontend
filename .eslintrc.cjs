937763f2fe3131fcf0dd82a3da27a8424e6595cd
deddy rahmat <deddyrahmat97@gmail.com>
2024-07-20 11:57:02 +0700

    update setup eslint
    update and fixed setup for eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,36 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-        "prettier",
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react-hooks/recommended',
+    // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
+    // Make sure it's always the last config, so it gets the chance to override other configs.
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
     ],
-    ignorePatterns: ["dist", ".eslintrc.cjs"],
-    parser: "@typescript-eslint/parser",
-    plugins: ["react-refresh"],
-    rules: {
-        "react-refresh/only-export-components": [
-            "warn",
-            { allowConstantExport: true },
-        ],
+  },
+  overrides: [
+    {
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+      files: ['./**/*.ts', './**/*.tsx'],
     },
+  ],
 };
--- src/__tests__/pages/App.test.tsx
+++ src/__tests__/pages/App.test.tsx
@@ -1,14 +1,14 @@
-import { describe, it, expect, test } from "vitest";
-import { render } from "@testing-library/react";
-import App from "../../App";
+import { describe, it, expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import App from '../../App';
 
-test("demo", () => {
-    expect(true).toBe(true);
+test('demo', () => {
+  expect(true).toBe(true);
 });
 
-describe("render", () => {
-    it("renders the main page", () => {
-        render(<App />);
-        expect(true).toBeTruthy();
-    });
+describe('render', () => {
+  it('renders the main page', () => {
+    render(<App />);
+    expect(true).toBeTruthy();
+  });
 });
