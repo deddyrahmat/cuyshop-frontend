386b7c0faa948377f758f83493c6dbbd63ebe062
deddy rahmat <deddyrahmat97@gmail.com>
2024-07-20 22:55:09 +0700

    setup husky and lint-staged
    setup husky and lint-staged for code auto clean

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
     // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
     // Make sure it's always the last config, so it gets the chance to override other configs.
     'prettier',
