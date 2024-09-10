6178d3caddbd640ae56ba0f8d5b80a9a31b7cd60
deddy rahmat <deddyrahmat97@gmail.com>
2024-09-10 12:14:43 +0700

    feat: confirmation logout
    make alert for confirmation logout

--- src/components/organisms/Header/useHeaderLogic.ts
+++ src/components/organisms/Header/useHeaderLogic.ts
@@ -8,6 +8,7 @@ import { USER_LOGOUT } from "../../../redux/authSlice";
 import { RESET_ADDRESS_STATE } from "../../../redux/addressSlice";
 import { RESET_CART_STATE } from "../../../redux/cartSlice";
 import { isEmpty } from "../../../utils/array/CheckValueEmpty";
+import Swal from "sweetalert2";
 
 const useHeaderLogic = () => {
   const dispatch = useAppDispatch();
@@ -35,14 +36,37 @@ const useHeaderLogic = () => {
     }
   }, [fetched, dispatch]);
 
-  const processLogout = async () => {
-    const res = await handleLogout();
-    if (res?.status) {
-      dispatch(USER_LOGOUT());
-      dispatch(RESET_ADDRESS_STATE());
-      dispatch(RESET_CART_STATE());
-      navigate("/", { replace: true });
-    }
+  const processLogout = () => {
+    Swal.fire({
+      icon: "warning",
+      title: "Keluar",
+      text: "Apakah kamu ingin keluar?",
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      padding: "2em",
+      customClass: {
+        confirmButton: "customButtonSwal",
+      },
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await handleLogout();
+        if (res?.status) {
+          dispatch(USER_LOGOUT());
+          dispatch(RESET_ADDRESS_STATE());
+          dispatch(RESET_CART_STATE());
+          navigate("/", { replace: true });
+        }
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        // Swal.fire('Cancelled', 'Your imaginary file is safe :)', 'error');
+        Swal.fire({
+          icon: "warning",
+          title: "Batal!",
+          text: "Kamu tetap login",
+          showConfirmButton: false,
+          timer: 3000,
+        });
+      }
+    });
   };
 
   return {
