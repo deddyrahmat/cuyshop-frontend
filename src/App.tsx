33f5b6091ac606463274c43ac1bcda933d0daecf
deddy rahmat <deddyrahmat97@gmail.com>
2024-09-10 10:45:46 +0700

    fix: file app
    remove ; in app

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
   return (
     <>
       <Provider store={storeRedux}>
-        <RouterProvider router={router} />;
+        <RouterProvider router={router} />
       </Provider>
     </>
   );
